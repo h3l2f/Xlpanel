7616952a546b1fc75c2f2db13ff73223e33edfbb
h3l2f <trmhoang2001@gmail.com>
2025-06-07 17:16:08 +0700

    fix shitta

--- routes/panel.py
+++ routes/panel.py
@@ -1,6 +1,7 @@
 # type: ignore
 from __main__ import *
 
+@app.route("/panel/")
 @app.route("/panel/<a>")
 def gtpn(a:str=None):
     if a: return redirect(config["pterodactyl"]["host"]+f"/server/{a}")
