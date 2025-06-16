de1fa2815a0daa303ab200204a1b69712c698973
h3l2f <trmhoang2001@gmail.com>
2025-06-16 23:13:43 +0700

    fck git

--- ende.py
+++ ende.py
@@ -2,21 +2,12 @@ import bcrypt
 import hashlib
 
 def hash(inp):
-<<<<<<< HEAD
     if (not inp): inp=""
     input_bytes = inp.encode('utf-8')
     sha256_hash = hashlib.sha256()
     sha256_hash.update(input_bytes)
     hashed_string = sha256_hash.hexdigest()
     return hashed_string
-=======
-	if inp == None: inp=""
-	input_bytes = inp.encode('utf-8')
-	sha256_hash = hashlib.sha256()
-	sha256_hash.update(input_bytes)
-	hashed_string = sha256_hash.hexdigest()
-	return hashed_string
->>>>>>> 61c988cc4f6ec69c2e602301988ed27d607fd054
 
 def encode(passwd):
 	bytes = passwd.encode('utf-8')
